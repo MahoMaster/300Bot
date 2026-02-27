38ee4c96e86f0a6d0b0e1b4a9d47a912af6ff898
文铭 <MahoMaster@163.com>
2026-02-27 15:06:52 +0800

    chore: 删除已弃用的Windows可执行文件与配置文件
    移除遗留的Windows启动脚本、可执行文件及go-cqhttp相关配置文件。这些文件已不再使用，清理以保持代码库整洁。

--- open_gocqhttp.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd "./conf/QQbot"
-call "./go-cqhttp_windows_amd64.exe"
--- windows_open.bat
+++
@@ -1,4 +0,0 @@
-timeout /t 1 /nobreak >nul
-start open_gocqhttp.bat
-timeout /t 20 /nobreak >nul
-call "./300Bot.exe"
